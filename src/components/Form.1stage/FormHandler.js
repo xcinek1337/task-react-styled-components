de34b212bdace0ca0447cc633dee4547e048e46f
Marcin <xcinek1337@wp.pl>
2023-10-31 15:18:30 +0100

    removed not used import uuid

--- src/components/Form.1stage/FormHandler.js
+++ src/components/Form.1stage/FormHandler.js
@@ -1,7 +1,6 @@
 import React, { useReducer, useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import validate from './helper';
-import { v4 as uuidv4 } from 'uuid';
 
 import { FormContainer, FormInput, FormLabel, FormSubmit } from './FormHandler.styled';
 
